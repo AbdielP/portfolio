4ea2285f0d43d9f797c2af23372a3bf7bb73dc2f
AbdielP <abdiel.pinzonc@gmail.com>
2025-04-15 05:32:06 -0500

    images fix and web title changed

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -29,7 +29,7 @@ export const ProjectSection = () => {
       title: 'Key Lookup & QR Labeling Tool',
       description:
         'A React-based application to streamline key management with Excel-to-MySQL import and QR code generation.',
-      image: '/images/dashboard_llaves.png',
+      image: 'images/dashboard_llaves.png',
       demo: '',
       code: 'https://github.com/CWB-Datacenter/dashboard-llaves',
       tech: ['React', 'MUI', 'PHP', 'MySQL'],
@@ -38,7 +38,7 @@ export const ProjectSection = () => {
       title: 'QR Codes Application',
       description:
         'QR code generator for datacenter access and registration, with Excel data and label generation.',
-      image: '/images/dashboard_qraccess.png',
+      image: 'images/dashboard_qraccess.png',
       demo: '',
       code: 'https://github.com/AbdielP/qrcodeproject',
       tech: ['Angular', 'Node.js', 'MySQL'],
@@ -47,7 +47,7 @@ export const ProjectSection = () => {
       title: 'SNMP Manager',
       description:
         'A tool to monitor temperature and humidity via SNMP-enabled sensors with a simple web interface.',
-      image: '/images/dashboard_snmp.png',
+      image: 'images/dashboard_snmp.png',
       demo: '',
       code: 'https://github.com/AbdielP/snmp_manager',
       tech: ['JavaScript', 'Node.js'],
